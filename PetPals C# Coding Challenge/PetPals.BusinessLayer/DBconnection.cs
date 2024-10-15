ab28564a71d0ed196f35420ad84cecec5836d1fe
Vijaykarthik-S <131847128+Vijaykarthik-S@users.noreply.github.com>
2024-10-15 13:14:15 +0530

    Add files via upload

---
+++ PetPals C# Coding Challenge/PetPals.BusinessLayer/DBconnection.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System;
+
+namespace PetPals.BusinessLayer.DBUtil
+{
+    public static class DBConnection
+    {
+        public static string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["MyDBConnection"].ConnectionString;
+        }
+    }
+}
